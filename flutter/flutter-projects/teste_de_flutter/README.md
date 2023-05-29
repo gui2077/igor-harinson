85d7a45b686dc168a2cbab7375778996a0f71d2f
Luís Guilherme <guigas0608@gmail.com>
2023-05-29 17:26:34 -0300

    primeiro arquivo

--- flutter/flutter-projects/teste_de_flutter/README.md
+++
@@ -1,3 +0,0 @@
-# teste_de_flutter
-
-A new Flutter project.
--- flutter/flutter-projects/teste_de_flutter/android/app/src/main/kotlin/com/example/teste_de_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.teste_de_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
